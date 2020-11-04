c5e14767a55bd6d39aac6aa7e167cddc34150ea4
Mordan1311 <daniloarenales1311@gmail.com>
2020-11-04 16:30:05 -0500

    "este proyecto aun está en desarrollo"

---
+++ ing_sw_2020_2/src/ing_sw_2020_2/Ing_sw_2020_2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ing_sw_2020_2;
+
+/**
+ *
+ * @author josem
+ */
+public class Ing_sw_2020_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos ");
+        System.out.println("Integrando Netbeans y Github");
+    }
+    
+}
